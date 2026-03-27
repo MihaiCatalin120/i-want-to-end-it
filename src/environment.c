0296642525cd7391527349b14a7b86b50f667ac0
MihaiCatalin120 <mihaicatalin449@gmail.com>
2026-03-27 17:21:15 +0200

    Adjust level variable names

--- src/environment.c
+++ src/environment.c
@@ -1,37 +1,37 @@
 #include "environment.h"
 
-Platform level0Platforms[] = {{{0, 400, 800, 200}, true, GRAY},
+Platform level1Platforms[] = {{{0, 400, 800, 200}, true, GRAY},
                               {{200, 200, 400, 10}, true, GRAY},
                               {{100, 300, 100, 10}, true, GRAY},
                               {{600, 300, 100, 10}, true, GRAY}};
 
-Spike level0Spikes[] = {
+Spike level1Spikes[] = {
     {{400, 200}, {450, 200}, {425, 175}}, {{350, 200}, {400, 200}, {375, 175}},
     {{450, 400}, {500, 400}, {475, 375}}, {{400, 400}, {450, 400}, {425, 375}},
     {{350, 400}, {400, 400}, {375, 375}}, {{300, 400}, {350, 400}, {325, 375}},
 };
 
-Platform level1Platforms[] = {
+Platform level2Platforms[] = {
     {{0, 500, 800, 100}, true, GRAY},  {{0, 200, 50, 300}, true, GRAY},
     {{50, 300, 50, 200}, true, GRAY},  {{300, 300, 100, 10}, true, GRAY},
     {{500, 200, 100, 10}, true, GRAY}, {{500, 400, 100, 10}, true, GRAY},
 };
 
-Spike level1Spikes[] = {
+Spike level2Spikes[] = {
     {{100, 500}, {150, 500}, {125, 475}},
 };
 const Level levels[] = {
     {
         (Vector2){50.0f, 280.0f},
-        level0Platforms,
-        sizeof(level0Platforms) / sizeof(level0Platforms[0]),
-        level0Spikes,
-        sizeof(level0Spikes) / sizeof(level0Spikes[0]),
-    },
-    {
-        (Vector2){10.0f, 200.0f},
         level1Platforms,
         sizeof(level1Platforms) / sizeof(level1Platforms[0]),
         level1Spikes,
         sizeof(level1Spikes) / sizeof(level1Spikes[0]),
+    },
+    {
+        (Vector2){10.0f, 200.0f},
+        level2Platforms,
+        sizeof(level2Platforms) / sizeof(level2Platforms[0]),
+        level2Spikes,
+        sizeof(level2Spikes) / sizeof(level2Spikes[0]),
     }};
